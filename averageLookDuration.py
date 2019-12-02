79bf203a495d2fedf8b8e470c3600db539d22371
Todd Calnan <todd.calnan@duke.edu>
2019-12-02 16:18:10 -0500

    Figured out how to access nested structures in a mat file in Python. Need to cycle through each condition for each subject next.

--- averageLookDuration.py
+++ averageLookDuration.py
@@ -5,7 +5,6 @@ Created on Tue Nov 26 12:00:04 2019
 @author: tmc54
 """
 import os
-import numpy as np
 from scipy.io import loadmat
 
 
@@ -14,12 +13,19 @@ os.chdir(path)
 
 listOfSubjectFolders = os.listdir('.')
 numberOfSubjects = len(listOfSubjectFolders)
-for subjectNumber in range(0,1):
-    subjectName = listOfSubjectFolders[subjectNumber]
-    os.chdir(path)
-    os.chdir(subjectName)
-    subjectFolderContents = os.listdir('.')
-    if subjectFolderContents: 
-        fileName = subjectName + '_EGT_Analysis.mat' 
-        analysisFile = loadmat(fileName)
+for subjectNumber in range(0,1): 
+    subjectName = listOfSubjectFolders[subjectNumber] # get subject name
+    os.chdir(path) # cd into the path
+    os.chdir(subjectName) # cd into the subject's folder
+    subjectFolderContents = os.listdir('.') # list the contents of the subject's folder
+    if subjectFolderContents: # if there are files in the subject's folder
+        fileName = subjectName + '_EGT_Analysis.mat' # name of the mat file for the subject
+        analysisFile = loadmat(fileName) # load the subject's mat file
+        datFileName = 'dat_' + subjectName # the name of the dat struct where the important data is saved
+        data = analysisFile[datFileName] # look at just the 'dat_*' struct in the mat file
+        segmentNames = data.dtype.names # list of segment names for this subject, 21 segments expected
+        segmentData = {n: data[n][0, 0] for n in segmentNames} # go through the dat file, pull out data for each segment, make a dictionary
+        for segmentNumber in range(0,len(segmentNames)): # go through each segment
+            gazeOnMedia = segmentData[segmentNames[segmentNumber]]
         
+            #db1_gazeMedia = data['DB1'][0,0]['gazeonmedia'][0,0]
