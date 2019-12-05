f900630b4a8ce454246d01e54a8df8da60c54fc5
Todd Calnan <todd.calnan@duke.edu>
2019-12-05 12:05:36 -0500

    For the moment, just pulling out gaze on media data for each segment, then converting the dictionary to a list of lists in chronological order (DB1, then sandwich1, etc). Next step is finding nans in a reasonable way.

--- averageLookDuration.py
+++ averageLookDuration.py
@@ -6,7 +6,7 @@ Created on Tue Nov 26 12:00:04 2019
 """
 import os
 from scipy.io import loadmat
-
+import numpy as np
 
 path = 'C:/Users/tmc54/Documents/EyeTracking/DukeACT/T2Background'
 os.chdir(path)
@@ -24,8 +24,10 @@ for subjectNumber in range(0,1):
         datFileName = 'dat_' + subjectName # the name of the dat struct where the important data is saved
         data = analysisFile[datFileName] # look at just the 'dat_*' struct in the mat file
         segmentNames = data.dtype.names # list of segment names for this subject, 21 segments expected
-        segmentData = {n: data[n][0, 0] for n in segmentNames} # go through the dat file, pull out data for each segment, make a dictionary
-        for segmentNumber in range(0,len(segmentNames)): # go through each segment
-            gazeOnMedia = segmentData[segmentNames[segmentNumber]]
-        
-            #db1_gazeMedia = data['DB1'][0,0]['gazeonmedia'][0,0]
+        gazeOnMedia = {n: data[n][0,0]['gazeonmedia'][0,0] for n in segmentNames} # go through the dat file, pull out data for each segment, make a dictionary
+        chronologicalSegmentOrder = ['DB1', 'sandwich1', 'DB2', 'JAmoose', 'DB3', 'JArooster', 'DB4', 'JAmonkey', 'DB5', 'JApenguin', 'DB6', 'sandwich2', 'DB7', 'MTmonkey', 'DB8', 'MTpenguin', 'DB9', 'MTrooster', 'DB10', 'MTmoose', 'DB11'];
+        socialSegmentOrder = ['DB1', 'DB2', 'DB3', 'DB4', 'DB5', 'DB6', 'DB7', 'DB8', 'DB9', 'DB10', 'DB11']
+        chronologicalGazeOnMedia = [gazeOnMedia[m] for m in chronologicalSegmentOrder]
+        nanLocations = [];
+        for m in range(0,len(chronologicalGazeOnMedia)):
+            nanLocations = np.argwhere(np.isnan(chronologicalGazeOnMedia[m])) #need to make nanLocations a list of lists as well
