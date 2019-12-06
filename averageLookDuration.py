b0eb76908ec11b4c091f966c32b2577d3ff24468
Todd Calnan <todd.calnan@duke.edu>
2019-12-06 11:24:09 -0500

    Found the index of samples where we have gaze points, so now we can find where disengagement periods are. We are only looking at the first column of gazeonmedia since if the subject looks away, both eyes show up as NaNs.

--- averageLookDuration.py
+++ averageLookDuration.py
@@ -8,9 +8,16 @@ import os
 from scipy.io import loadmat
 import numpy as np
 
+## Set up
+chronologicalSegmentOrder = ['DB1', 'sandwich1', 'DB2', 'JAmoose', 'DB3', 'JArooster', 'DB4', 'JAmonkey', 'DB5', 'JApenguin', 'DB6', 'sandwich2', 'DB7', 'MTmonkey', 'DB8', 'MTpenguin', 'DB9', 'MTrooster', 'DB10', 'MTmoose', 'DB11'];
+socialSegmentOrder = ['DB1', 'DB2', 'DB3', 'DB4', 'DB5', 'DB6', 'DB7', 'DB8', 'DB9', 'DB10', 'DB11']
+toleranceThreshold = 24 # number of samples allowed before we call it a distraction
+
+## Set up the path
 path = 'C:/Users/tmc54/Documents/EyeTracking/DukeACT/T2Background'
 os.chdir(path)
 
+## Go through all subjects
 listOfSubjectFolders = os.listdir('.')
 numberOfSubjects = len(listOfSubjectFolders)
 for subjectNumber in range(0,1): 
@@ -24,10 +31,8 @@ for subjectNumber in range(0,1):
         datFileName = 'dat_' + subjectName # the name of the dat struct where the important data is saved
         data = analysisFile[datFileName] # look at just the 'dat_*' struct in the mat file
         segmentNames = data.dtype.names # list of segment names for this subject, 21 segments expected
-        gazeOnMedia = {n: data[n][0,0]['gazeonmedia'][0,0] for n in segmentNames} # go through the dat file, pull out data for each segment, make a dictionary
-        chronologicalSegmentOrder = ['DB1', 'sandwich1', 'DB2', 'JAmoose', 'DB3', 'JArooster', 'DB4', 'JAmonkey', 'DB5', 'JApenguin', 'DB6', 'sandwich2', 'DB7', 'MTmonkey', 'DB8', 'MTpenguin', 'DB9', 'MTrooster', 'DB10', 'MTmoose', 'DB11'];
-        socialSegmentOrder = ['DB1', 'DB2', 'DB3', 'DB4', 'DB5', 'DB6', 'DB7', 'DB8', 'DB9', 'DB10', 'DB11']
-        chronologicalGazeOnMedia = [gazeOnMedia[m] for m in chronologicalSegmentOrder]
-        nanLocations = [];
-        for m in range(0,len(chronologicalGazeOnMedia)):
-            nanLocations = np.argwhere(np.isnan(chronologicalGazeOnMedia[m])) #need to make nanLocations a list of lists as well
+        gazeOnMedia = {n: data[n][0,0]['gazeonmedia'][0,0] for n in segmentNames} # go through the dat file, pull out data for each segment, make a dictionary. 
+        chronologicalGazeOnMedia = [gazeOnMedia[m][:,0] for m in chronologicalSegmentOrder] # [:,0] is so we just pull out the first column of gazeonmedia
+        noNanLocations = [np.argwhere(~np.isnan(chronologicalGazeOnMedia[m])) for m in range(0,len(chronologicalGazeOnMedia))] # find where there are gaze points in each segment, so we can see where the subject looks away
+        ## Find nonconsecutive values in noNanLocations, so we can find the disengage points
+        
